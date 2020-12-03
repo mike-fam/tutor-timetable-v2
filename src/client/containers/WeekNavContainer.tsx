eaf8a2e6e14c139ce3032a9febe2230c1fb7caa5
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-12-03 20:06:55 +1000

    initial commit

---
+++ src/client/containers/WeekNavContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export const WeekNavContainer: React.FunctionComponent<Props> = () => {
+    return (
+        <div>
+            <h1>week navigation test</h1>
+        </div>
+    );
+};
