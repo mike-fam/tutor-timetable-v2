2f88c3402cf60064ba0834de2fa16c2d9042f2b8
Mike <mikepham1207@gmail.com>
2020-12-05 00:09:01 +1000

    Move ChakraProvider to index.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Dropdown } from "./components/Dropdown";
-import { ChakraProvider } from "@chakra-ui/react";
 
 const App = () => {
     return (
-        <ChakraProvider>
-            <Dropdown onChange={(e) => console.log(e.target.value)}>
-                {["s2 2019", "s1 2020", "s2 2020", "s1 2021"]}
-            </Dropdown>
-        </ChakraProvider>
+        <Dropdown onChange={(e) => console.log(e.target.value)}>
+            {["s2 2019", "s1 2020", "s2 2020", "s1 2021"]}
+        </Dropdown>
     );
 };
 
