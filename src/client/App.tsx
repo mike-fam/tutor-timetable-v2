ec8d2a9af08d67898b28974361514393884cfa1d
Mike <mikepham1207@gmail.com>
2020-12-05 15:14:14 +1000

    Wrapper component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Dropdown } from "./components/Dropdown";
+import { Wrapper } from "./components/Wrapper";
 
 const App = () => {
     return (
-        <Dropdown onChange={(e) => console.log(e.target.value)}>
-            {["s2 2019", "s1 2020", "s2 2020", "s1 2021"]}
-        </Dropdown>
+        <Wrapper>
+            <Dropdown onChange={(e) => console.log(e.target.value)}>
+                {["s2 2019", "s1 2020", "s2 2020", "s1 2021"]}
+            </Dropdown>
+        </Wrapper>
     );
 };
 
