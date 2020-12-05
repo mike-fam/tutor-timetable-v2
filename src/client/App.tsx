cee26f5dc12817018bca0404038b01e8dd2d851e
Joshua Zhou <joshua_zhou1999@hotmail.com>
2020-12-05 22:40:29 +1000

    Removed dividers

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -10,9 +10,9 @@ const App = () => {
             <Dropdown onChange={(e) => console.log(e.target.value)}>
                 {["s2 2019", "s1 2020", "s2 2020", "s1 2021"]}
             </Dropdown>
-            <Divider />
+            <br></br>
             <CourseSelectContainer />
-            <Divider />
+            <br></br>
             <WeekNavContainer />
         </div>
     );
