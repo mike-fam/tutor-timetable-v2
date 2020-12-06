dbd1000bc7b10c9c4cb0389097b2b398fd7faa38
Mike <mikepham1207@gmail.com>
2020-12-07 00:55:52 +1000

    TimeSlot is now dark mode compatible

--- src/client/components/timetable/TimeSlot.tsx
+++ src/client/components/timetable/TimeSlot.tsx
@@ -1,18 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 
 type Props = {};
 
-export const TimeSlot: React.FC<Props> = ({}) => {
+export const TimeSlot: React.FC<Props> = () => {
+    const evenStripes = useColorModeValue("lightgrey", "#1A365D");
+    const oddStripes = useColorModeValue("darkgrey", "#2A4365");
     return (
         <Box
             w="100%"
             h="100%"
             _even={{
-                bgImage: `repeating-linear-gradient(-45deg, lightgrey, lightgrey 1px, transparent 1px, transparent 4px)`,
+                bgImage: `repeating-linear-gradient(-45deg, ${evenStripes}, ${evenStripes} 1px, transparent 1px, transparent 4px)`,
             }}
             _odd={{
-                bgImage: `repeating-linear-gradient(-45deg, darkgrey, darkgrey 1px, transparent 1px, transparent 4px)`,
+                bgImage: `repeating-linear-gradient(-45deg, ${oddStripes}, ${oddStripes} 1px, transparent 1px, transparent 4px)`,
             }}
         />
     );
