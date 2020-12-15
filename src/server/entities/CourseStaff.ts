cc6d61bdeda98fd66c9e9171da401ddaf02d5677
Mike <mikepham1207@gmail.com>
2020-12-15 21:57:13 +1000

    One-to-one relationship between Preference and CourseStaff

--- src/server/entities/CourseStaff.ts
+++ src/server/entities/CourseStaff.ts
@@ -1,8 +1,18 @@
-import { BaseEntity, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+    BaseEntity,
+    Entity,
+    ManyToOne,
+    OneToOne,
+    PrimaryGeneratedColumn,
+    Unique,
+} from "typeorm";
 import { Timetable } from "./Timetable";
 import { User } from "./User";
+import { Preference } from "./Preference";
+import { JoinColumn } from "typeorm/browser";
 
 @Entity()
+@Unique(["timetable", "user"])
 export class CourseStaff extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
@@ -12,4 +22,8 @@ export class CourseStaff extends BaseEntity {
 
     @ManyToOne(() => User, (user) => user.courseStaffs)
     user: User;
+
+    @OneToOne(() => Preference, (preference) => preference.courseStaff)
+    @JoinColumn()
+    preference: Preference;
 }
--- src/server/entities/Preference.ts
+++ src/server/entities/Preference.ts
@@ -3,14 +3,16 @@ import {
     Check,
     Column,
     Entity,
+    OneToOne,
     PrimaryGeneratedColumn,
 } from "typeorm";
 import { SessionType } from "../../types/session";
 import { checkFieldValueInEnum } from "../utils/query";
+import { CourseStaff } from "./CourseStaff";
 
+@Entity()
 // Session type is one of the types specified.
 @Check(checkFieldValueInEnum(SessionType, "sessionType"))
-@Entity()
 export class Preference extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
@@ -23,4 +25,7 @@ export class Preference extends BaseEntity {
 
     @Column()
     maxWeeklyHours: number;
+
+    @OneToOne(() => CourseStaff, (courseStaff) => courseStaff.preference)
+    courseStaff: CourseStaff;
 }
