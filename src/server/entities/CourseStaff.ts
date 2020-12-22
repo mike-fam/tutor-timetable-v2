dd49eac7b98c976bb3269b3fd35b6f1886960bc7
Mike <mikepham1207@gmail.com>
2020-12-23 01:39:34 +1000

    Course Staff now keeps track of roles

--- src/server/entities/CourseStaff.ts
+++ src/server/entities/CourseStaff.ts
@@ -6,30 +6,43 @@ import {
     PrimaryGeneratedColumn,
     Unique,
     JoinColumn,
+    Column,
+    Check,
 } from "typeorm";
 import { Timetable } from "./Timetable";
 import { User } from "./User";
 import { Preference } from "./Preference";
 import { Field, Int, ObjectType } from "type-graphql";
+import { Role } from "../../types/user";
+import { checkFieldValueInEnum } from "../utils/query";
 
 @ObjectType()
 @Entity()
+@Check(checkFieldValueInEnum(Role, "role"))
 @Unique(["timetable", "user"])
 export class CourseStaff extends BaseEntity {
     @Field(() => Int)
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Field(() => Role)
+    @Column("varchar")
+    role: Role;
+
     @Field(() => Timetable)
-    @ManyToOne(() => Timetable, (timetable) => timetable.courseStaffs)
+    @ManyToOne(() => Timetable, (timetable) => timetable.courseStaffs, {
+        eager: true,
+    })
     timetable: Timetable;
 
     @Field(() => User)
     @ManyToOne(() => User, (user) => user.courseStaffs)
     user: User;
 
-    @Field(() => Preference)
-    @OneToOne(() => Preference, (preference) => preference.courseStaff)
+    @Field(() => Preference, { nullable: true })
+    @OneToOne(() => Preference, (preference) => preference.courseStaff, {
+        nullable: true,
+    })
     @JoinColumn()
     preference: Preference;
 }
