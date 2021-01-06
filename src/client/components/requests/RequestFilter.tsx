e1e38d2928d79095af9c4108b8d1253426df8444
Mike <mikepham1207@gmail.com>
2021-01-06 16:22:05 +1000

    Remove invalid import

--- src/client/components/requests/RequestFilter.tsx
+++ src/client/components/requests/RequestFilter.tsx
@@ -1,7 +1,6 @@
 import { Box, Center, Checkbox, Stack, Tooltip } from "@chakra-ui/react";
 import React from "react";
 import { FilterType } from "../../containers/requests/RequestContainer";
-import { SimpleCheckboxList } from "../SimpleCheckboxList";
 
 type Props = {
     setFilters: (item: FilterType) => void;
