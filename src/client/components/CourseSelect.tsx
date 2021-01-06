f42d167879669224dee9fb77e5519477fa6fc5ec
Mike <mikepham1207@gmail.com>
2021-01-06 16:20:31 +1000

    Revert "Generalise CourseSelect component to CheckboxList"
    This reverts commit 5b7f2fa7a3541953bca0ff7431a20122b26ec315.

---
+++ src/client/components/CourseSelect.tsx
@@ -0,0 +1,51 @@
+import { Checkbox, Stack } from "@chakra-ui/react";
+import React from "react";
+
+// PLaceholder name for the function callback.
+type Props = {
+    courses: Array<String>;
+    callbackFunc: Function;
+};
+
+export const CourseSelect: React.FunctionComponent<Props> = (props: Props) => {
+    // Maybe use a Map here instead?
+    const [checkedItems, setCheckedItems] = React.useState(
+        new Array<boolean>(props.courses.length).fill(false)
+    );
+
+    const allChecked = checkedItems.every(Boolean);
+
+    const checkItems = (index: number, value: boolean, course: String) => {
+        let temp: Array<boolean> = [...checkedItems];
+        temp[index] = value;
+        props.callbackFunc(course, value);
+        setCheckedItems(temp);
+    };
+
+    return (
+        <div>
+            {/*Onchange might need to be modified here depending on callback logic*/}
+            <Checkbox
+                isChecked={allChecked}
+                onChange={() => {
+                    setCheckedItems(checkedItems.map((x) => !allChecked));
+                }}
+            >
+                All Courses
+            </Checkbox>
+            <Stack pl={6} mt={1} spacing={1}>
+                {props.courses.map((course, index) => (
+                    <Checkbox
+                        isChecked={checkedItems[index]}
+                        onChange={() => {
+                            checkItems(index, !checkedItems[index], course);
+                        }}
+                        key={index}
+                    >
+                        {course}
+                    </Checkbox>
+                ))}
+            </Stack>
+        </div>
+    );
+};
