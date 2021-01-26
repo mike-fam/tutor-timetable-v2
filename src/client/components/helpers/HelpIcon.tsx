63102362cdefe6945cc536b4095f6d43a108271d
Mike <mikepham1207@gmail.com>
2021-01-27 01:23:11 +1000

    HelpIcon helper component

---
+++ src/client/components/helpers/HelpIcon.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+    Box,
+    Icon,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverContent,
+    PopoverTrigger,
+} from "@chakra-ui/react";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+
+type Props = {};
+
+export const HelpIcon: React.FC<Props> = ({ children }) => {
+    return (
+        <Popover placement="right-start" trigger="hover">
+            <PopoverTrigger>
+                <Box>
+                    <Icon as={BsFillQuestionCircleFill} />
+                </Box>
+            </PopoverTrigger>
+            <PopoverContent>
+                <PopoverArrow />
+                <PopoverBody>{children}</PopoverBody>
+            </PopoverContent>
+        </Popover>
+    );
+};
