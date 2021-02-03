4e2b8bbb077ce20a8c57b45be569501339a1e0ff
Mike <mikepham1207@gmail.com>
2021-02-03 16:45:36 +1000

    Fix course staff value

--- src/client/components/helpers/FormikRadioGroup.tsx
+++ src/client/components/helpers/FormikRadioGroup.tsx
@@ -1,4 +1,4 @@
-import { FormControl, FormLabel, Radio } from "@chakra-ui/react";
+import { FormLabel, Radio, RadioGroup } from "@chakra-ui/react";
 import { capitalCase } from "change-case";
 import { Field, FieldProps } from "formik";
 import React from "react";
@@ -11,15 +11,17 @@ type Props = {
 
 export const FormikRadioGroup: React.FC<Props> = ({ name, values, label }) => {
     return (
-        <FormControl name={name}>
+        <>
             <FormLabel>{label || capitalCase(name)}</FormLabel>
-            {values.map((value) => (
-                <Field name={name}>
-                    {({ field }: FieldProps) => (
-                        <Radio {...field}>{value}</Radio>
-                    )}
-                </Field>
-            ))}
-        </FormControl>
+            <Field name={name}>
+                {({ field }: FieldProps) => (
+                    <RadioGroup {...field}>
+                        {values.map((value) => (
+                            <Radio>{value}</Radio>
+                        ))}
+                    </RadioGroup>
+                )}
+            </Field>
+        </>
     );
 };
