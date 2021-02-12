29ed96147f3aac04ba4f42ec00dbdefa6dbb8b22
Mike <mikepham1207@gmail.com>
2021-02-12 19:50:10 +1000

    Catch all routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import "./config";
-import express, { Express } from "express";
+import express, { Express, Response } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { createServer } from "http";
 import { buildSchema } from "type-graphql";
@@ -23,6 +23,7 @@ import { PreferenceResolver } from "./resolvers/PreferenceResolver";
 import { AllocatorResolver } from "./resolvers/AllocatorResolver";
 import { StaffRequestResolver } from "./resolvers/StaffRequestResolver";
 import { CourseResolver } from "./resolvers/CourseResolver";
+import * as path from "path";
 
 const main = async () => {
     await createConnection(ormconfig);
@@ -39,13 +40,6 @@ const main = async () => {
         })
     );
     app.use("/", express.static("build/client"));
-
-    // Catch-all route
-    // app.use("*", (_, res: Response) => {
-    //     res.sendFile("index.html", {
-    //         root: "build",
-    //     });
-    // });
 
     app.use(asyncHandler(uqAuthMiddleware));
 
@@ -71,6 +65,14 @@ const main = async () => {
     });
 
     apolloServer.applyMiddleware({ app });
+
+    // Catch-all route
+    app.use("*", (_, res: Response) => {
+        res.sendFile("index.html", {
+            root: path.resolve("./build", "client"),
+        });
+    });
+
     server.listen(port, () => {
         console.log(`Listening on port ${port}`);
     });
