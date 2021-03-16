eeaf89815aa71705dc1b8bc92cc2a979fb10f09d
Mike <mikepham1207@gmail.com>
2021-03-17 08:29:11 +1000

    Send mail function

--- src/server/mailer/index.ts
+++ src/server/mailer/index.ts
@@ -1,9 +1,26 @@
 import { createTransport } from "nodemailer";
 
+const sender = "noreply.tutortimetable@gmail.com";
+
 export const transporter = createTransport({
     service: "gmail",
     auth: {
-        user: "noreply.tutortimetable@gmail.com",
+        user: sender,
         pass: process.env.EMAIL_PW,
     },
 });
+
+export const sendMail = async (
+    recipient: string,
+    cc: string[],
+    subject: string,
+    content: string
+) => {
+    await transporter.sendMail({
+        from: sender,
+        to: recipient,
+        cc,
+        subject,
+        text: content,
+    });
+};
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,9 +1,9 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    DB_URL: string;
-    CORS_ORIGIN: string;
-    ALLOCATOR_URL: string;
-    EMAIL_PW: string;
-    NODE_ENV: "development" | "production";
-  }
+    interface ProcessEnv {
+        DB_URL: string;
+        CORS_ORIGIN: string;
+        ALLOCATOR_URL: string;
+        EMAIL_PW: string;
+        NODE_ENV: "development" | "production";
+    }
 }
