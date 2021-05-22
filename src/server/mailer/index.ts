e3f8c40e019566737b30a7f5ce9d8978a175af9c
Mike <mikepham1207@gmail.com>
2021-05-22 16:11:17 +1000

    Update with very simple mail template

--- src/server/mailer/index.ts
+++ src/server/mailer/index.ts
@@ -1,26 +1,24 @@
 import { createTransport } from "nodemailer";
 
-const sender = "noreply.tutortimetable@gmail.com";
-
-export const transporter = createTransport({
-    service: "gmail",
-    auth: {
-        user: sender,
-        pass: process.env.EMAIL_PW,
+const transporter = createTransport({
+    host: "mailhub.eait.uq.edu.au",
+    port: 25,
+    secure: false,
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 
-export const sendMail = async (
-    recipient: string,
-    cc: string[],
-    subject: string,
-    content: string
-) => {
-    await transporter.sendMail({
-        from: sender,
-        to: recipient,
-        cc,
-        subject,
-        text: content,
-    });
+const message = {
+    from: {
+        name: "Q NoReply",
+        address: "noreply@q.uqcloud.net",
+    },
+    to: "b.pham@uqconnect.edu.au",
+    bcc: "mike.pham@uq.edu.au",
+    subject: "Message title",
+    text: "Plaintext version of the message",
+    html: "<p>HTML version of the message</p>",
 };
+
+transporter.sendMail(message);
--- src/server/mailer/template.ts
+++ src/server/mailer/template.ts
@@ -24,5 +24,5 @@ export const newRequestTemplate = `
 </p>
 <p>Click <a href="https://tutor-timetable.uqcloud.net/requests">here</a> to view
  the request and make a new offer.</p>
- <p>This message is auto-generated and no one is monitoring this email. Do not reply.</p>
+<strong>This message is auto-generated and no one is monitoring this email. Do not reply.</strong>
 `;
