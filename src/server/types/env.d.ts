2194db4a0b9f002620d6f407e278d46750c4c9d9
Mike <mikepham1207@gmail.com>
2021-06-08 18:41:03 +1000

    Env type and .env.example files

---
+++ src/server/types/env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DB_URL: string;
+    CORS_ORIGIN: string;
+    ALLOCATOR_URL: string;
+    EMAIL_PW: string;
+    NODE_ENV: string;
+    MAIL_SERVER: string;
+    MAIL_PORT: string;
+  }
+}
