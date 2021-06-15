17a66a7e706ef0f3b617b7a8ed840f0b2c1beccf
Mike <mikepham1207@gmail.com>
2021-06-15 12:13:34 +1000

    Seed postgres db docker compose

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    InMemoryCache,
+    split,
+    from,
+} from "@apollo/client";
 import { WrapperContainer } from "./containers/WrapperContainer";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { RetryLink } from "@apollo/client/link/retry";
+
+const httpLink = new HttpLink({
+    uri: "/graphql",
+});
+
+const retryLink = new RetryLink();
+
+const wsLink = new WebSocketLink({
+    uri: "/graphql",
+    options: {
+        reconnect: true,
+    },
+});
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+        );
+    },
+    wsLink,
+    from([retryLink, httpLink])
+);
 
 const client = new ApolloClient({
-    uri: "/graphql",
+    link: splitLink,
     cache: new InMemoryCache({
         addTypename: false,
         typePolicies: {
