0014b037ea37590d043f9b2baac6fccf43e73d31
Mike <31090859+mike-fam@users.noreply.github.com>
2021-06-23 09:34:40 +1000

    ITT-33:ADD Update boilerplate files to include jest and seed data

--- src/server/types/env.d.ts
+++ src/server/types/env.d.ts
@@ -1,14 +1,18 @@
 declare namespace NodeJS {
-    interface ProcessEnv {
-        DB_HOST: string;
-        DB_USER: string;
-        DB_PW: string;
-        DB_NAME: string;
-        CORS_ORIGIN: string;
-        ALLOCATOR_URL: string;
-        EMAIL_PW: string;
-        NODE_ENV: string;
-        MAIL_SERVER: string;
-        MAIL_PORT: string;
-    }
+  interface ProcessEnv {
+    DB_HOST: string;
+    DB_USER: string;
+    DB_PW: string;
+    DB_NAME: string;
+    TEST_DB_HOST: string;
+    TEST_DB_USER: string;
+    TEST_DB_PW: string;
+    TEST_DB_NAME: string;
+    CORS_ORIGIN: string;
+    ALLOCATOR_URL: string;
+    EMAIL_PW: string;
+    NODE_ENV: string;
+    MAIL_SERVER: string;
+    MAIL_PORT: string;
+  }
 }
