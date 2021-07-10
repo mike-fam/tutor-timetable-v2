6df8322ab876f56fa46575a17cec30ef02e0a838
Mike Pham <31090859+mike-fam@users.noreply.github.com>
2021-07-10 14:50:56 +1000

    ITT-36 Logic for converting week patterns

---
+++ src/client/utils/session-stream.ts
@@ -0,0 +1,25 @@
+export const weeksPatternRepr = (weekNames: string[], weeks: number[]) => {
+    const weekRanges: string[] = [];
+    const sortedWeeks = [...weeks].sort((a, b) => a - b);
+    let lastWeek: number | undefined = undefined;
+    let firstConsecutiveWeek: number | undefined = undefined;
+    for (const week of sortedWeeks) {
+        if (!lastWeek || !firstConsecutiveWeek) {
+            lastWeek = firstConsecutiveWeek = week;
+            continue;
+        }
+        if (week - 1 !== lastWeek) {
+            let weekRange: string;
+            if (firstConsecutiveWeek !== lastWeek) {
+                weekRange = `${weekNames[firstConsecutiveWeek]} - ${weekNames[lastWeek]}`;
+            } else {
+                weekRange = `${weekNames[lastWeek]}`;
+            }
+            weekRanges.push(weekRange);
+            firstConsecutiveWeek = week;
+        }
+
+        lastWeek = week;
+    }
+    return weekRanges.join(", ");
+};
