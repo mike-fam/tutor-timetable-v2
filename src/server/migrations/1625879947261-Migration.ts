0f1ff80b06b71ef6ef7262044016ca6d68d48326
Mike <31090859+mike-fam@users.noreply.github.com>
2021-07-12 16:24:58 +1000

    Migrations for fixing database inconsistencies

--- src/server/migrations/1625879947261-Migration.ts
+++ src/server/migrations/1625879947261-Migration.ts
@@ -6,10 +6,10 @@ export class Migration1625879947261 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE IF EXISTS ONLY public.session_stream_allocated_users_user DROP CONSTRAINT IF EXISTS "FK_e62630c379f91fe30d920b34626"`);
         await queryRunner.query(`ALTER TABLE IF EXISTS ONLY public.session_stream_allocated_users_user DROP CONSTRAINT IF EXISTS "FK_76fe690bd48a4cd9aa58bfcb455"`);
-        await queryRunner.query(`ALTER TABLE IF EXISTS ONLY public.session_stream_allocated_users_user DROP CONSTRAINT IF EXISTS "PK_4ac2d5c51b2a40fab3d090c5d4d"`);
-        await queryRunner.query(`DROP TABLE IF EXISTS public.session_stream_allocated_users_user`);
         await queryRunner.query(`DROP INDEX IF EXISTS public."IDX_76fe690bd48a4cd9aa58bfcb45"`);
         await queryRunner.query(`DROP INDEX IF EXISTS public."IDX_e62630c379f91fe30d920b3462"`);
+        await queryRunner.query(`ALTER TABLE IF EXISTS ONLY public.session_stream_allocated_users_user DROP CONSTRAINT IF EXISTS "PK_4ac2d5c51b2a40fab3d090c5d4d"`);
+        await queryRunner.query(`DROP TABLE IF EXISTS public.session_stream_allocated_users_user`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
